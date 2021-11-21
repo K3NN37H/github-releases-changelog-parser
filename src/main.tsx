60e8763b5ab4ad352e365243d48b8fa50072595c
Ken <kjtchan@gmail.com>
2021-11-20 19:21:26 -0500

    Use octokit for Github api
    - Sticky the TOC
    - Config to stop at a version, limit page size

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,44 @@
 import ReactMarkdown from 'react-markdown'
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
+import { Octokit } from '@octokit/core'
+import { throttling } from '@octokit/plugin-throttling'
+import { paginateRest } from '@octokit/plugin-paginate-rest'
 
-const breakingReg = /breaking/
+const pluggedClient = Octokit.plugin(throttling, paginateRest)
+const ghClient = new pluggedClient({
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit) => {
+      octokit.log.warn(`Rate limit hit ${options.url}`);
 
-const repoReg = "[\\w-]+\\/[\\w-]+"
+      if (options.request.retryCount === 0) {
+        octokit.log.info(`Retry after ${retryAfter} seconds`)
+        return true
+      }
+    },
+    onAbuseLimit: (retryAfter, options, octokit) => {
+      octokit.log.warn(`Abuse limit hit ${options.url}`)
+    }
+  }
+})
+
+const breakingReg = /breaking/i
+
+const repoReg = "[-\\w.]+/[-\\w.]+"
 
 type ReleasesData = {
-  name: string
+  name: string | null
   tag_name: string
-  body: string
+  body?: string | null
 }
 
 type DataState = 'init' | 'loading' | 'success' | 'error'
 
 export default function App() {
-  const [repo, setRepo] = React.useState('')
-  const [data, setData] = React.useState<ReleasesData[] | null>(null)
+  const [repo, setRepo] = useState('')
+  const [data, setData] = useState<ReleasesData[] | null>(null)
+  const [numberOfReleases, setNumberOfReleases] = useState(100)
+  const [stopAtVersion, setStopAtVersion] = useState('')
+  const [pagingLimit, setPagingLimit] = useState(10)
   const [dataState, setDataState] = React.useState<DataState>('init')
 
   const onSubmit = (event) => {
@@ -23,30 +46,25 @@ export default function App() {
     event.stopPropagation()
     console.log('sending')
     if (!repo) return
+
     const doFetch = async () => {
-      // webpack or formium/formik
-      const resp = await fetch(`https://api.github.com/repos/${repo}/releases?per_page=35`, { headers: { 'Accept': 'application/vnd.github.v3+json' } })
-      const query =
-`query {
-  repository(owner:"webpack", name:"webpack") {
-    releases(last:20) {
-      edges {
-        node {
-          name
-          tagName
-          description
-          descriptionHTML
+      // webpack or formium/formik or octokit/request.js
+      let currentPage = 1
+      const [owner, repoName] = repo.split('/')
+      const respData = await ghClient.paginate('GET /repos/{owner}/{repo}/releases', {
+        owner,
+        repo: repoName,
+        per_page: numberOfReleases
+      }, (response, done) => {
+        if (response.data.find(({ name, tag_name }) => stopAtVersion && (tag_name === stopAtVersion || name === stopAtVersion))) {
+          done()
         }
-      }
-    }
-  }
-}`
-// const bodyContent = { query }
-// const resp = await fetch(`https://api.github.com/graphql`, {
-//   method: 'post',
-//   body: JSON.stringify(bodyContent)
-// })
-      const respData = await resp.json()
+        else if (currentPage++ >= pagingLimit) {
+          done()
+        }
+        return response.data
+      })
+
       console.log('Response', respData)
       setDataState('success')
       setData(respData)
@@ -58,7 +76,7 @@ export default function App() {
 
   const toc = useMemo(() => {
     if (data) {
-      return data.filter(release => breakingReg.test(release.body))
+      return data.filter(release => breakingReg.test(release.body || ''))
         .map(release =>
           <li key={`toc-${release.tag_name}`}>
             <a href={`#${release.tag_name}`}>{release.tag_name}</a>
@@ -74,7 +92,7 @@ export default function App() {
         (release) =>
           <div key={release.tag_name}>
             <h1 id={release.tag_name}>{release.tag_name}</h1>
-            <ReactMarkdown children={release.body} />
+            <ReactMarkdown children={release.body || ''} />
           </div>
       )
     }
@@ -83,15 +101,23 @@ export default function App() {
 
   return (
     <>
-    <form onSubmit={onSubmit}>
-      <label htmlFor='repo'>Owner/repo</label>
-      <input type='text' id='repo' pattern={repoReg} required onChange={event => setRepo(event.target.value)} />
-      <input type='submit' />
+      <form onSubmit={onSubmit}>
+        <label htmlFor='repo'>Owner/repo</label>
+        <input type='text' id='repo' placeholder='owner/repo' pattern={repoReg} required onChange={event => setRepo(event.target.value)} />
+        <label htmlFor='releases'>Number of releases per page</label>
+        <input type='range' id='releases' min={1} max={100} value={numberOfReleases} onChange={event => setNumberOfReleases(parseInt(event.target.value, 10))} />
+        {numberOfReleases}
+        <label htmlFor='stopversion'>Oldest version to stop at</label>
+        <input type='text' id='stopversion' onChange={event => setStopAtVersion(event.target.value)} />
+        <input type='submit' />
       </form>
-      <ul>
-        {toc}
-      </ul>
-      {dataState === 'loading' && <span>Loading</span>}
+      <div className='toc'>
+        <h2>Releases with potentially breaking changes</h2>
+        <ul>
+          {toc}
+        </ul>
+      </div>
+      {dataState === 'loading' && <span>Loading...</span>}
       {cl}
     </>
   )
