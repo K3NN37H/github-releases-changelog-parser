caf87b078df7507962addc53bfeff7f05800f520
Ken <kjtchan@gmail.com>
2024-03-09 00:58:40 -0500

    Fix build to output correct path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(async () => ({
   plugins: [react()],
   root: 'src',
+  base: './',
   build: {
     outDir: '../docs',
     emptyOutDir: true
